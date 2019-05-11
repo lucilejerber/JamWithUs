45574ee106878b58b71265b3894b2c9a03a3cf3f
Timanmay <laura.sergiro@gmail.com>
2019-05-11 15:08:57 +0200

    Laura_livraison_mai

--- reactNative/JamWithUs/screens/HomeScreen.js
+++ reactNative/JamWithUs/screens/HomeScreen.js
@@ -17,7 +17,41 @@ export default class HomeScreen extends React.Component {
     header: null,
   };
 
-  render() {
+  componentWillMount(){ // fait des trucs sur la page en arriere plan a l'ouverture par exemple appel BDD
+    fetch('http://938455a2.ngrok.io', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json'
+      },
+    })
+    .then(json => console.log(json))
+    //.then(json => this.setState({
+      //  name: json.name
+      //}))
+    .catch(error => console.error(error))
+
+    {/*fetch('https://d7642d77.ngrok.io/Jam/save', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        ContentType: 'application/json'
+      },
+      body: JSON.stringify({
+        "date": this.state.date,
+        "nom": this.state.name,
+        "complet": null,
+        "nombreMaxParticipants": 0,
+        "nombreDeParticipants": 0,
+        "description": this.state.description,
+        "administrateur": "admin",
+        "lieu": this.state.lieu
+      }),
+    })
+    .then(json => console.log(json))
+    .catch(error => console.error(error))*/}
+  }
+
+  render() { //affiche des trucs sur la page
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
@@ -42,7 +76,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
+              Change this text and your app will automatically reload. 
             </Text>
           </View>
 
