32fe5fc4ff8547bbde7f97cdd01cc5a4d0cf0f70
Gael <gael.andrianirina@isep.fr>
2019-06-04 11:46:14 +0200

    Update App.js

--- reactNative/JamWithUs/App.js
+++ reactNative/JamWithUs/App.js
@@ -1,24 +1,13 @@
+
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 
 import AppNavigator from './navigation/AppNavigator';
-import DrawerNavigator from './navigation/DrawerNavigator';
-
 
 export default class App extends React.Component {
 
   render() {
       return (
-          <View style={styles.container}>
-          	<DrawerNavigator/>
-          </View>
+          <AppNavigator/>
       )
-   }
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-}) 
