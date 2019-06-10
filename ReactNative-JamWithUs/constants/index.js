d9627539f668a2193d0b74549792f99cc60c93e0
Timanmay <laura.sergiro@gmail.com>
2019-06-10 17:32:45 +0200

    Styles

--- ReactNative-JamWithUs/constants/index.js
+++ ReactNative-JamWithUs/constants/index.js
@@ -2,4 +2,6 @@
 export const NGROK = "https://3939cabf.ngrok.io"
 
 // Serveur Link
-export const TOMCAT = 'http://projets-tomcat.isep.fr:8080/JamWithUs-0.1/user/save'
+export const TOMCATSAVE = 'http://projets-tomcat.isep.fr:8080/JamWithUs-0.1/user/save'
+export const TOMCATUPDATE = 'http://projets-tomcat.isep.fr:8080/JamWithUs-0.1/user/update'
+export const TOMCATCREATE = 'http://projets-tomcat.isep.fr:8080/JamWithUs-0.1/user/create'
